4e4f0b9fc67be1b206f9bc23a7941806849a93ad
Swarna <dslatha13@gmail.com>
2023-09-30 19:45:28 +0530

    first commit

--- app.js
+++ app.js
@@ -2,12 +2,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 // Create an Express application
 
 app.use(bodyParser.json());
+app.use(cors());
 
 // Create a MySQL connection pool (replace with your database config)
 const pool = mysql.createPool({
@@ -43,14 +44,53 @@ app.post('/register', (req, res) => {
       }
     });
 });
+// Define a GET endpoint to retrieve phone numbers
+app.get('/getPhoneNumber', (req, res) => {
+    const { phoneNumber } = req.query.phoneNumber;
+    console.log('Query Parameters:', req.query.phoneNumber);
+    // Validate the phoneNumber format (you can use your validation logic)
+   /* if (!validatePhoneNumber(phoneNumber)) {
+      return res.status(400).json({ error: 'Invalid phone number format' });
+    }*/
+  
+    // Query the database to retrieve the phone number
 
+    pool.getConnection((err, connection) => {
+        if (err) {
+          console.error('Error connecting to MySQL:', err);
+          res.status(500).json({ message: 'Database error. Please try again later.' });
+        } else {
+            console.log("Connection Success");
+            const query = 'SELECT mobilenumber FROM register WHERE mobilenumber = ?';
+    
+            connection.query(query, [req.query.phoneNumber], (err, results) => {
+      if (err) {
+        console.error('Error executing query: ', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+  
+      if (results.length === 0) {
+        return res.status(404).json({ error: 'Phone number not found' });
+      }
+       console.log('results length = ' + results)
+      const foundPhoneNumber = results[0].mobilenumber;
+      console.log("foundPhoneNumber = "+ foundPhoneNumber);
+      res.json({ phoneNumber: foundPhoneNumber });
+    });
+               
+        } 
+});
+});
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
 
-
+function validatePhoneNumber(phoneNumber) {
+    // Implement your validation logic here
+    return true; // Return true for demonstration purposes
+  }
 
 
 
